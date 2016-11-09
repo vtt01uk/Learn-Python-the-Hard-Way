818db2dcc03e894ab83d8784313be9a4908d9c69
vtt01uk <vtt01uk@gmail.com>
2016-11-09 09:34:52 +0000

    Exercise 7 completed

---
+++ ex7.py
@@ -0,0 +1,40 @@
+# Prints a statement
+print "Mary had a little lamb."
+# Prints a statement with formatter %s that has a value of 'snow'
+print "Its fleece was white as %s." % 'snow'
+# Prints a statement
+print "And everywhere that Mary went."
+# Prints . 10 times
+print "." * 10 # what'd that do?
+
+# Assigns variable end1 with a value
+end1 = "C"
+# Assigns variable end2 with a value
+end2 = "h"
+# Assigns variable end3 with a value
+end3 = "e"
+# Assigns variable end4 with a value
+end4 = "e"
+# Assigns variable end5 with a value
+end5 = "s"
+# Assigns variable end6 with a value
+end6 = "e"
+# Assigns variable end7 with a value
+end7 = "B"
+# Assigns variable end8 with a value
+end8 = "u"
+# Assigns variable end9 wtih a value
+end9 = "r"
+# Assigns variable end10 with a value
+end10 = "g"
+# Assigns variable end11 with a value
+end11 = "e"
+# Assigns variable end12 with a value
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# Concatenates the variables and prints them
+print end1 + end2 + end3 + end4 + end5 + end6,
+# Concatenates the variables and prints them 
+print end7 + end8 + end9 + end10 + end11 + end12
+ 
